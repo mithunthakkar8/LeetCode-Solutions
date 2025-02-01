18869a49ed8dc633c7b3ff1161949e67c1d07f2a
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-02-01 08:08:48 +0530

    Update String to Binary.py

--- Python/Binary Operations/String to Binary.py
+++ Python/Binary Operations/String to Binary.py
@@ -1,6 +1,6 @@
 class Solution:
     # Method to add two binary numbers represented as strings
     def addBinary(self, a: str, b: str) -> str:
-        # Convert binary strings 'a' and 'b' to decimal using int(), add them, then convert the sum back to binary using bin().
+        # Convert binary strings 'a' and 'b' to decimal using int(value,2), add them, then convert the sum back to binary using bin().
         # The bin() function returns a string starting with '0b', so we slice [2:] to remove the '0b' prefix and return the binary result.
         return bin(int(a, 2) + int(b, 2))[2:]
