664f8432033ef783f4aa4819098b5778f578b8db
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-02-12 20:08:06 +0530

    Delete Python/String Operations/Majority Element.py

--- Python/String Operations/Majority Element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        # Sort the given list of numbers
-        sorted_nums = sorted(nums)
-        
-        # The majority element always appears more than ⌊n/2⌋ times,
-        # so after sorting, it will always be at index len(nums) // 2
-        return sorted_nums[len(nums) // 2]
