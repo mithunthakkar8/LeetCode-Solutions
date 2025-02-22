a159e8e46c347fb1b8e593a0cb5ae0b1a81612f9
mithunthakkar8 <122671015+mithunthakkar8@users.noreply.github.com>
2025-02-22 07:42:46 +0530

    Create Bank Account Summary II.sql

---
+++ SQL/Accounting Specific/Bank Account Summary II.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select name
+    , sum(amount) as balance
+from users u
+join transactions t
+on u.account = t.account
+group by name
+having sum(amount)>10000
